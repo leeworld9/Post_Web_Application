0a68482fe31d402cd6deb110e444fa578a82c15a
dohelee <leeworld9@gmail.com>
2022-05-08 14:32:05 +0900

    feat: Add Posts Service(Find/Update/Save) & Test Code

---
+++ src/main/java/com/example/demo/web/PostsApiController.java
@@ -0,0 +1,38 @@
+package com.example.demo.web;
+
+import com.example.demo.service.posts.PostsService;
+import com.example.demo.web.dto.PostsResponseDto;
+import com.example.demo.web.dto.PostsSaveRequestDto;
+import com.example.demo.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    /*
+    스프링에서 Bean을 주입하는 방법으로는 `@Autowired`, `setter`, `생성자` 와 같은 방식이 있으며,
+    흔히들 사용하는 `@Autowired`로 Bean을 주입하는 방법은 권장되지 않고 있음.
+
+    권장되는 방법은 `생성자`로 주입하는 방법이며, 롬복의 `@RequiredArgsConstructor` 어노테이션을 사용하면
+    롬복이 final이 선언된 모든 필드의 생성자를 대신 생성해줘서 간편하게 이용할 수 있음.
+    또한 롬복의 어노테이션의 장점으로는 생성자 코드를 계속해서 수정하는 번거로움을 해결할 수 있음.
+     */
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
---
+++ src/test/java/com/example/demo/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.example.demo.domain.posts;
+
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+// 수동 추가 (자동으로 import가 안됬음...)
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest // 별다른 설정 없이 @SpringBootTest를 사용할 경우 H2 데이터베이스를 자동으로 실행해 줍니다.
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After	// Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
