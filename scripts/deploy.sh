6a89c301e94816530f77cb9c94f528c2b2bfd323
dohelee <leeworld9@gmail.com>
2022-05-20 08:23:31 +0900

    update: Revert codedeploy config & Update deploy.sh
    스크립트(deploy.sh)가 배포 후에 실행되지 않아서 실행권한 문제라고 생각했으나 실제로 AWS Codedeploy EC2 Log를 확인해본 결과 해당 파일의 실행권한 문제는 아님. 추가로 deploy.sh의 스크립트 내부에서 jar 파일의 권한을 수정해주는 부분에 오타가 발생하여 수정하였음.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,7 +23,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $JAR_NAME 2
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
