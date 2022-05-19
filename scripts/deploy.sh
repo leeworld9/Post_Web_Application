e71368e285dc336904acc7931e9ac971fa8b65e0
dohelee <leeworld9@gmail.com>
2022-05-19 18:47:27 +0900

    fix: spring-session error Update: update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,4 +31,7 @@ fi
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
-nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties $REPOSITORY/$JAR_NAME 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties, classpath:/application-real.properties, \
+        /home/ec2-user/app/application-oauth.properties, home/ec2-user/app/application-real-db.properties \
+ -Dspring.profiles.active=real \
+ $REPOSITORY/$JAR_NAME 2>&1 &
