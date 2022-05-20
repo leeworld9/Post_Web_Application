5dd2bee9c9ba47602586cd28780319f2efce2678
dohelee <leeworld9@gmail.com>
2022-05-21 01:44:08 +0900

    feat: Add Zero-Downtime Deployment

--- src/main/java/com/example/demo/web/ProfileController.java
+++ src/main/java/com/example/demo/web/ProfileController.java
@@ -16,7 +16,7 @@ public class ProfileController {
     @GetMapping("/profile")
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles()); // 현재 실행 중인 ActiveProfile을 모두 가져옵니다. (real, oauth, real-db)
-        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2"); // 무중단 배포(step3)에서는 real1과 real2만 사용되지만, step2를 다시 사용해볼 수도 있으니 real도 남겨둡니다.
         String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
         return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
     }
